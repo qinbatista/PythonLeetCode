cac5cdc69897aef2bf76e5d526e8c590327b152c
qin.batista <qin.batista@outlook.com>
2023-04-16 15:07:38 -0700

    update

--- Stack.py
+++ Stack.py
@@ -27,16 +27,51 @@ class Stack:
     def getMin(self) -> int:
         return self.minStack[-1]
 
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for str in tokens:
+            if str == "+":
+                stack.append(stack.pop()+stack.pop())
+            elif str == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b-a)
+            elif str == "*":
+                stack.append(stack.pop()*stack.pop())
+            elif str == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b/a))
+            else:
+                stack.append(int(str))
+        return stack[-1]
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closeN):
+            if openN == closeN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1, closeN)
+                stack.pop()
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN, closeN+1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
+
 
 if __name__ == "__main__":
     s = Stack()
-    s.push(-2)
-    s.push(0)
-    s.push(-3)
-    print(s.getMin())
-    s.pop()
-    s.top()
-    print(s.getMin())
-    # print(tp.isPalindrome("0P"))
-    # print(tp.twoSum([2,3,4], 6))
-    # print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    # s.push(-2)
+    # s.push(0)
+    # s.push(-3)
+    # print(s.getMin())
+    # s.pop()
+    # s.top()
+    # print(s.getMin())
+    # print(s.evalRPN(["2", "1", "+", "3", "*"]))
+    print(s.generateParenthesis(3))
